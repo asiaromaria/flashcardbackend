24c69993e62f5681328a4098f92ae19da827fa4e
asiaromaria <designsbyarc@gmail.com>
2021-08-11 16:23:25 -0700

    took out mongo connection string and imported cards, set up port listener, and imported express.

---
+++ index.js
@@ -0,0 +1,15 @@
+const connectDB = require('./startup/db');
+const express = require('express');
+const app = express();
+
+const cards = require('./routes/cards');
+
+connectDB();
+
+app.use(express.json());
+app.use('/api/cards', cards);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
