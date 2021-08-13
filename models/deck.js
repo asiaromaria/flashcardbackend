632039d51cd398862263852b3f685ec3ba7c4ea0
asiaromaria <designsbyarc@gmail.com>
2021-08-12 19:24:26 -0700

    commented out everything b/c moved everything to cards models.

--- models/deck.js
+++ models/deck.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose');
-const Joi = require ('joi');
+// const mongoose = require('mongoose');
+// const Joi = require ('joi');
 
 
 
-const deckSchema = new mongoose.Schema({
-    name:  { type: String, required: true, minlength: 2, maxlength: 255 },
-    description: { type: String, required: true, minlength: 2, maxlength: 255 },
-    level: { type: String, required: true },
-    dateModified: { type: Date, default: Date.now },
-});
+// const deckSchema = new mongoose.Schema({
+//     name:  { type: String, required: true, minlength: 2, maxlength: 255 },
+//     description: { type: String, required: true, minlength: 2, maxlength: 255 },
+//     level: { type: String, required: true },
+//     dateModified: { type: Date, default: Date.now },
+// });
 
-const Deck = mongoose.model('Deck', deckSchema);
+// const Deck = mongoose.model('Deck', deckSchema);
 
-function validateDeck(deck) {
-    const schema = Joi.object({
-        name: Joi.string().min(2).max(50).required(),
-        description: Joi.string().required(),
-        level: Joi.string().required(),
-    });
-    return schema.validate(deck);
-}
+// function validateDeck(deck) {
+//     const schema = Joi.object({
+//         name: Joi.string().min(2).max(50).required(),
+//         description: Joi.string().required(),
+//         level: Joi.string().required(),
+//     });
+//     return schema.validate(deck);
+// }
 
-exports.Deck = Deck; 
-exports.validate = validateDeck; 
-exports.deckSchema = deckSchema;
+// exports.Deck = Deck; 
+// exports.validate = validateDeck; 
+// exports.deckSchema = deckSchema;
