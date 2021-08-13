c4fc3dff1ecafa05a386889da51165682bd3038f
asiaromaria <designsbyarc@gmail.com>
2021-08-12 19:25:05 -0700

    updated the validate method for both card and deck, updated url endpoint.

--- routes/deck.js
+++ routes/deck.js
@@ -1,25 +1,108 @@
-const { Card, validate } = require('../models/cards');
-const Deck = require('../models/deck');
+const { Card, validateC } = require('../models/cards');
+const { Deck, validateD } = require('../models/cards');
 const express = require('express');
 const router = express.Router();
 
 //All endpoints and routen handlers go here.
 
+  //deck CRUD
 router.get('/', async (req, res) => {
-    try {
-      const cards = await Card.find();
-      return res.send(cards);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-  });
+  try {
+    const decks = await Deck.find();
+    return res.send(decks);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+//expecting an id to be passed into the requestor’s URL endpoint. 
+router.get('/:deckId', async (req, res) => {
+  try {
+    const deck = await Deck.findById(req.params.deckId);
+    if (!deck)
+      return res.status(400).send(`The product with id "${req.params.deckId}" does not exist.`);
+    return res.send(deck);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateD(req.body);
+    if (error) return res.status(400).send(error);
+
+    const deck = new Deck({
+      name: req.body.name,
+      description: req.body.description,
+      level: req.body.level,
+    });
+    await deck.save();
+
+    return res.send(deck);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+router.put('/:deckId', async (req, res) => {
+  try {
+    const { error } = validateD(req.body);
+    if (error) return res.status(400).send(error);
+
+    const deck = await Deck.findByIdAndUpdate(
+      req.params.deckId,
+      {
+        name: req.body.name,
+        description: req.body.description,
+        level: req.body.level,
+      },
+      { new: true }
+    );
+    if (!deck)
+      return res.status(400).send(`The deck with id "${req.params.deckId}" does not exist.`);
+    
+    await deck.save();
+
+    return res.send(deck);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+router.delete('/:deckId', async (req, res) => {
+  try {
+    const deck = await Deck.findByIdAndRemove(req.params.deckId);
+    if (!deck)
+      return res.status(400).send(`The deck with id "${req.params.deckId}" does not exist.`);
+    
+    return res.send(deck);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+
+
+//CARD CRUD
+
+  router.get('/:deckId/cards', async (req, res) => {
+      try {
+        const cards = await Card.find();
+        return res.send(cards);
+      } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+      }
+    });
   
+
   //expecting an id to be passed into the requestor’s URL endpoint. 
-  router.get('/:id', async (req, res) => {
+  router.get('/:deckId/cards/:cardId', async (req, res) => {
     try {
-      const card = await Card.findById(req.params.id);
+      const card = await Card.findById(req.params.cardId);
       if (!card)
-        return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
+        return res.status(400).send(`The product with id "${req.params.cardId}" does not exist.`);
       return res.send(card);
     } catch (ex) {
       return res.status(500).send(`Internal Server Error: ${ex}`);
@@ -27,9 +110,9 @@ router.get('/', async (req, res) => {
   });
   
   
-  router.post("/", async (req, res) => {
+  router.post("/:deckId/cards", async (req, res) => {
     try {
-      const { error } = validate(req.body);
+      const { error } = validateC(req.body);
       if (error) return res.status(400).send(error);
   
       const card = new Card({
@@ -46,9 +129,9 @@ router.get('/', async (req, res) => {
     }
   });
   
-  router.put('/:id', async (req, res) => {
+  router.put('/:deckId/cards/:cardId', async (req, res) => {
     try {
-      const { error } = validate(req.body);
+      const { error } = validateC(req.body);
       if (error) return res.status(400).send(error);
   
       const card = await Card.findByIdAndUpdate(
@@ -62,7 +145,7 @@ router.get('/', async (req, res) => {
         { new: true }
       );
       if (!card)
-        return res.status(400).send(`The card with id "${req.params.id}" does not exist.`);
+        return res.status(400).send(`The card with id "${req.params.cardId}" does not exist.`);
       
       await card.save();
   
@@ -72,11 +155,11 @@ router.get('/', async (req, res) => {
     }
   });
   
-  router.delete('/:id', async (req, res) => {
+  router.delete('/:deckId/cards/:cardId', async (req, res) => {
     try {
-      const card = await Card.findByIdAndRemove(req.params.id);
+      const card = await Card.findByIdAndRemove(req.params.cardId);
       if (!card)
-        return res.status(400).send(`The card with id "${req.params.id}" does not exist.`);
+        return res.status(400).send(`The card with id "${req.params.cardId}" does not exist.`);
       
       return res.send(card);
     } catch (ex) {
@@ -84,85 +167,7 @@ router.get('/', async (req, res) => {
     }
   });
 
-  //deck CRUD
 
-  router.get('/', async (req, res) => {
-    try {
-      const decks = await Deck.find();
-      return res.send(decks);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-  });
-  
-  //expecting an id to be passed into the requestor’s URL endpoint. 
-  router.get('/:id', async (req, res) => {
-    try {
-      const deck = await Deck.findById(req.params.id);
-      if (!deck)
-        return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
-      return res.send(deck);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-  });
-  
-  
-  router.post("/", async (req, res) => {
-    try {
-      const { error } = validate(req.body);
-      if (error) return res.status(400).send(error);
-  
-      const deck = new Deck({
-        name: req.body.name,
-        description: req.body.description,
-        category: req.body.category,
-        level: req.body.level,
-      });
-      await deck.save();
-  
-      return res.send(deck);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-  });
-  
-  router.put('/:id', async (req, res) => {
-    try {
-      const { error } = validate(req.body);
-      if (error) return res.status(400).send(error);
-  
-      const deck = await Deck.findByIdAndUpdate(
-        req.params.id,
-        {
-          name: req.body.name,
-          description: req.body.description,
-          category: req.body.category,
-          level: req.body.level,
-        },
-        { new: true }
-      );
-      if (!deck)
-        return res.status(400).send(`The deck with id "${req.params.id}" does not exist.`);
-      
-      await deck.save();
-  
-      return res.send(deck);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-  });
-  
-  router.delete('/:id', async (req, res) => {
-    try {
-      const deck = await Deck.findByIdAndRemove(req.params.id);
-      if (!deck)
-        return res.status(400).send(`The deck with id "${req.params.id}" does not exist.`);
-      
-      return res.send(deck);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-  });
+
 
 module.exports = router;
