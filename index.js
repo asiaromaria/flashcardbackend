bac3147d7034953036e472b69f8ce76884b1d5cb
asiaromaria <designsbyarc@gmail.com>
2021-08-16 20:29:24 -0700

    installed & imported cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // const cards = require('./routes/deck');
 const decks = require('./routes/deck');
 
 connectDB();
 
+
+app.use(cors())
 app.use(express.json());
 // app.use('/api/cards', cards);
 app.use('/api/decks', decks);
